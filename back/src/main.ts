bc182e75360592e7d8f2e5b1c1841effdb4de2ce
Almaz Balbaev <kujitengu@gmail.com>
2023-05-08 18:04:29 +0600

    Initial commit

---
+++ back/src/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { BadRequestException, ValidationError, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        const validationErrors: ValidationErrors = {};
+
+        errors.forEach((error) => {
+          const constraints = error.constraints;
+
+          if (constraints) {
+            validationErrors[error.property] = Object.keys(constraints).map((key) => constraints[key]);
+          }
+        });
+
+        return new BadRequestException(validationErrors);
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
+  await app.listen(8040);
+}
+bootstrap();
---
+++ front/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app';
+import { CssBaseline } from '@mui/material';
+import '@/styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
+}
