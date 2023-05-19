87ceec570e6dd3f52b5c463d36535d282e6ef624
Almaz Balbaev <kujitengu@gmail.com>
2023-05-19 14:04:36 +0600

    backend auth logic
    - added decorator for returning user from request
    - added token-auth.guard.ts

---
+++ back/src/auth/token-auth.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/user.entity';
+
+@Injectable()
+export class TokenAuthGuard implements CanActivate {
+  constructor(@InjectRepository(User) private readonly usersRepository: Repository<User>) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.get('Authorization');
+
+    if (!token) {
+      return false;
+    }
+
+    const user = await this.usersRepository.findOne({
+      where: {
+        token,
+      },
+    });
+
+    if (!user) {
+      return false;
+    }
+
+    request.user = user;
+
+    return true;
+  }
+}
