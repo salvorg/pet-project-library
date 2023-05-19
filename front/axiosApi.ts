8e0b8323b6ada0127a37de677ee98612d216cb78
Almaz Balbaev <kujitengu@gmail.com>
2023-05-19 13:43:33 +0600

    frontend dependencies added sass, sass-loader

--- front/axiosApi.ts
+++ front/axiosApi.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosHeaders, InternalAxiosRequestConfig } from 'axios';
 import { apiURL } from './constants';
-import { RootState } from '@/app/store';
+import { AppStore } from '@/app/store';
 import { Store } from '@reduxjs/toolkit';
 
 const axiosApi = axios.create({ baseURL: apiURL });
 
-export const addInterceptors = (store: Store<RootState>) => {
+export const addInterceptors = (store: Store<AppStore>) => {
   axiosApi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const token = store.getState().users.user?.token;
     const headers = config.headers as AxiosHeaders;
