27bd7658b83b3af8032a9b05b9b78bee961b6cb0
Almaz Balbaev <kujitengu@gmail.com>
2023-05-29 13:09:10 +0600

    backend: Database module update - added bookBorrowing entity

--- back/src/database/database.module.ts
+++ back/src/database/database.module.ts
@@ -5,6 +5,7 @@ import { Author } from '../authors/author.entity';
 import { Book } from '../books/book.entity';
 import { Genre } from '../genres/genre.entity';
 import { User } from '../users/user.entity';
+import { BookBorrowingEntity } from '../bookBorrowings/bookBorrowing.entity';
 
 @Module({
   imports: [
@@ -18,7 +19,7 @@ import { User } from '../users/user.entity';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD') as string,
         database: configService.get('DB_DATABASE'),
-        entities: [User, Author, Book, Genre],
+        entities: [User, Author, Book, Genre, BookBorrowingEntity],
         synchronize: true,
       }),
     }),
