4f1e26d10a005324f85f706531ba4a8637ef9343
Almaz Balbaev <kujitengu@gmail.com>
2023-05-30 19:40:38 +0600

    backend: migrations logic
    - created typeOrm.config.ts with connection to database for migrations
    - added scripts for generate/run/revert migrations

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,22 +2,45 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Author } from './authors/author.entity';
-import { Book } from './books/book.entity';
-import { Genre } from './genres/genre.entity';
-import { AuthorsController } from './authors/authors.controller';
-import { BooksController } from './books/books.controller';
-import { GenresController } from './genres/genres.controller';
-import { AuthorsService } from './authors/authors.service';
-import { BooksService } from './books/books.service';
-import { GenresService } from './genres/genres.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { User } from './users/user.entity';
+import { Author } from './routes/authors/author.entity';
+import { Book } from './routes/books/book.entity';
+import { Genre } from './routes/genres/genre.entity';
+import { AuthorsController } from './routes/authors/authors.controller';
+import { BooksController } from './routes/books/books.controller';
+import { GenresController } from './routes/genres/genres.controller';
+import { AuthorsService } from './routes/authors/authors.service';
+import { BooksService } from './routes/books/books.service';
+import { GenresService } from './routes/genres/genres.service';
+import { UsersController } from './routes/users/users.controller';
+import { UsersService } from './routes/users/users.service';
+import { User } from './routes/users/user.entity';
+import { GoogleBooksApiController } from './fixtures/googleBooksApi.controller';
+import { BookBorrowingEntity } from './routes/bookBorrowings/bookBorrowing.entity';
+import { SeedCommand } from './fixtures/seed.command';
+import { FixturesService } from './fixtures/fixtures.service';
+import { UsersFixtures } from './fixtures/users.fixtures';
+import { AuthorsFixtures } from './fixtures/authors.fixtures';
+import { GenresFixtures } from './fixtures/genres.fixtures';
+import { BooksFixtures } from './fixtures/books.fixtures';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, TypeOrmModule.forFeature([User, Author, Book, Genre])],
-  controllers: [UsersController, AuthorsController, BooksController, GenresController],
-  providers: [UsersService, AuthorsService, BooksService, GenresService],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    TypeOrmModule.forFeature([User, Author, Book, Genre, BookBorrowingEntity]),
+  ],
+  controllers: [UsersController, AuthorsController, BooksController, GenresController, GoogleBooksApiController],
+  providers: [
+    UsersService,
+    AuthorsService,
+    BooksService,
+    GenresService,
+    UsersFixtures,
+    AuthorsFixtures,
+    GenresFixtures,
+    BooksFixtures,
+    FixturesService,
+    SeedCommand,
+  ],
 })
 export class AppModule {}
---
+++ back/typeOrm.config.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+config();
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['*/**/*.entity.ts'],
+  migrations: ['src/database/migrations/*.ts'],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
+export default AppDataSource;
