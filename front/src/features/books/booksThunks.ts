ab741ed85e0771477eda785e30cc12e88fe098e9
Almaz Balbaev <kujitengu@gmail.com>
2023-05-31 17:18:16 +0600

    frontend: added get one book data request

--- front/src/features/books/booksThunks.ts
+++ front/src/features/books/booksThunks.ts
@@ -1,7 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axiosApi from '../../../axiosApi';
+import { BookApi } from '../../../types';
 
-export const fetchAllBooks = createAsyncThunk('books/fetchAll', async () => {
+export const fetchAllBooks = createAsyncThunk<BookApi[]>('books/fetchAll', async () => {
   const response = await axiosApi.get('/books');
+  return response.data;
+});
+
+export const fetchOneBook = createAsyncThunk<BookApi, string>('books/fetchOneBook', async (id) => {
+  console.log();
+  const response = await axiosApi.get(`/books/${Number(id)}`);
   return response.data;
 });
