a95ca8105a057baff460cbe295e7939a95a4ecff
Almaz Balbaev <kujitengu@gmail.com>
2023-05-31 17:40:22 +0600

    frontend: Genre form created
    - created genre creation form

---
+++ front/src/components/Forms/GenreForm.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Grid, TextField } from '@mui/material';
+
+interface Props {
+  onSubmit: (genre: string) => void;
+}
+
+const GenreForm: React.FC<Props> = ({ onSubmit }) => {
+  // const creating = useAppSelector(selectGenreCreating);
+  const [state, setState] = useState<string>('');
+
+  const submitFormHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(state);
+  };
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setState(value);
+  };
+
+  return (
+    <form autoComplete="off" onSubmit={submitFormHandler}>
+      <Grid container direction="column" spacing={2}>
+        <Grid item xs>
+          <TextField
+            fullWidth
+            id="name"
+            label="Name"
+            value={state}
+            onChange={inputChangeHandler}
+            name="name"
+            required
+          />
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default GenreForm;
