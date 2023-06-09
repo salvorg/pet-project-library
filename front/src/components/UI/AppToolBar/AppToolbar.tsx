59c868260adca97ce72ac672c31942a60cf84a3c
Almaz Balbaev <kujitengu@gmail.com>
2023-06-10 00:37:55 +0600

    Frontend: fixed bug with unclosed search div onClick

--- front/src/components/UI/AppToolBar/AppToolbar.tsx
+++ front/src/components/UI/AppToolBar/AppToolbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useAppSelector } from '@/app/hooks';
 import { selectUser } from '@/features/users/usersSlice';
 import { AppBar, Grid, Toolbar } from '@mui/material';
@@ -12,28 +12,35 @@ import Search from '@/components/UI/SearchDrawer/Search';
 const AppToolbar = () => {
   const user = useAppSelector(selectUser);
   const [showSearchState, setShowSearchState] = useState(false);
+  const searchRef = useRef(null);
+  const searchButtonRef = useRef(null);
 
   const handleToggleDiv = () => {
-    setShowSearchState(!showSearchState);
+    setShowSearchState((prevState) => !prevState);
   };
+
+  const handleClickOutside = (event) => {
+    if (
+      searchRef.current &&
+      !searchRef.current.contains(event.target) &&
+      !searchButtonRef.current.contains(event.target)
+    ) {
+      setShowSearchState(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
       <AppBar position="sticky" sx={{ backgroundColor: '#051a1d' }}>
         <Toolbar>
           <Grid container justifyContent="space-between" alignItems="center">
-            <IconButton
-              onClick={handleToggleDiv}
-              sx={{
-                color: '#f9f9f9',
-                fontSize: 18,
-                transition: 'font-size 0.7s',
-                '&:hover': { color: '#d0c4f4', fontSize: 21 },
-              }}
-            >
-              <SearchIcon />
-              Search
-            </IconButton>
             <Link href={'/'}>
               <img
                 src="https://seeklogo.com/images/P/pokeball-logo-DC23868CA1-seeklogo.com.png"
@@ -41,12 +48,27 @@ const AppToolbar = () => {
                 style={{ width: '50px', paddingTop: 5, alignSelf: 'center' }}
               />
             </Link>
+            <Grid item sx={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-end' }}>
+              <IconButton
+                ref={searchButtonRef}
+                onClick={handleToggleDiv}
+                sx={{
+                  color: '#f9f9f9',
+                  fontSize: 18,
+                  transition: 'font-size 0.7s',
+                  '&:hover': { color: '#d0c4f4', fontSize: 21 },
+                }}
+              >
+                <SearchIcon />
+                Search
+              </IconButton>
+            </Grid>
             <Grid item>{user ? <UsersMenu user={user} /> : <AnonymousMenu />}</Grid>
           </Grid>
         </Toolbar>
       </AppBar>
       {showSearchState && (
-        <Grid container>
+        <Grid container ref={searchRef}>
           <Search />
         </Grid>
       )}
