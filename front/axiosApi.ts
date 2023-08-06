cfd0a2816c3b906db1c9e7383c6a4e1d731024dc
Almaz Balbaev <kujitengu@gmail.com>
2023-08-06 23:18:40 +0600

    changed import from RootStore to RootState

--- front/axiosApi.ts
+++ front/axiosApi.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosHeaders, InternalAxiosRequestConfig } from 'axios';
 import { apiURL } from './constants';
-import { RootStore } from '@/app/store';
+import { RootState } from '@/app/store';
 import { Store } from '@reduxjs/toolkit';
 
 const axiosApi = axios.create({ baseURL: apiURL });
 
-export const addInterceptors = (store: Store<RootStore>) => {
+export const addInterceptors = (store: Store<RootState>) => {
   axiosApi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const token = store.getState().users.user?.token;
     const headers = config.headers as AxiosHeaders;
